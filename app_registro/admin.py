fd549982c037ee7dcbb81e75cd435ebce440caf4
Melva-99 <melvarobertina.83@outlook.com>
2021-06-02 23:36:28 -0600

    estructura del proyecto de conferencias

--- app_registro/admin.py
+++ app_registro/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from app_registro.models import Conferencia,Conferencista
-# Register your models here.
+from .models import Conferencia, Conferencista, Participante
 
+# ------------------------------------------------------------------------
 class ConferenciaAdmin(admin.ModelAdmin):
-    list_display = ('id','nombre', 'fecha', 'hora', 'conferencista')
+    list_display = ('id', 'nombre', 'fecha', 'hora', 'conferencista')
     list_editable = ('nombre', 'conferencista')
 
+admin.site.register(Conferencia, ConferenciaAdmin)
+# ------------------------------------------------------------------------
 admin.site.register(Conferencista)
-admin.site.register(Conferencia)
-
+# ------------------------------------------------------------------------
+admin.site.register(Participante)
--- app_registro/models.py
+++ app_registro/models.py
@@ -1,43 +1,50 @@
 from django.db import models
 
-
 '''
     Tipos de datos para los campos de los modelos:
-    -Todos los campos se obtienen de models
-    -Tipo CharField(max_lenght=25, default='Jack', null=true,blank=true)
+    - Todos los campos se obtienen de models
+    -> CharField(max_lenght=25, default='Jack', null=True, blank=True)
     -> TextField()
     -> IntegerField()
-    ->DecimalField(max_digits=5, decimal_places=2)
+    -> DecimalField(max_digits=5, decimal_places=2)
     -> PositiveIntegerField()
-    -> SamllIntegerField()
-    -> BooleanField(default=true)
+    -> SmallIntegerField()
+    -> BooleanField(default=True)
     -> EmailField()
     -> ImageField(upload_to='fotos')
     -> FileField(upload_to='archivos')
-    -> SlugField() : tesla ha afectado el precio del bitcoin tesla-ha-afectado-
-
-    Campos para establecer relaciones entre modelos:
-    >ForeignKey(Categoria,on_delete='models.CASCADE') 'models.SET_NULL'=valor huerfano on_delete=dependiendo de lo que pasa va ocurrir en el secundario
-    ->OneToOneField()
-    ->ManyToManyField()
-
+    -> SlugField(): Tesla ha afectado el precio del Bitcoin >> tesla-ha-afectado-....
+    Campos para establecer relaciones entre Modelos:
+    -> ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    -> OneToOneField()
+    -> ManyToManyField()
 '''
 
 class Conferencista(models.Model):
-    nombre = models.CharField(max_length=35)
-    apellido = models.CharField(max_length=35)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
     experiencia = models.TextField()
 
     def __str__(self):
         return self.nombre
 
-#guardar participantes en la base de datos
+
 class Conferencia(models.Model):
     nombre = models.CharField(max_length=35)
-    fecha_registro = models.DateTimeField(auto_now_add=True) #se agrega la fecha del sistema
+    fecha_registro = models.DateTimeField(auto_now_add=True)
     fecha = models.DateField()
     hora = models.TimeField()
-    conferencista = models.ForeignKey(Conferencista, on_delete=models.CASCADE, null=true)
+    conferencista = models.ForeignKey(Conferencista, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.nombre
+        return f'Conferencia; {self.nombre} - Conferencista: {self.conferencista}'
+
+
+class Participante(models.Model):
+    nombre = models.CharField(max_length=25)
+    apellido = models.CharField(max_length=25)
+    correo = models.EmailField()
+    twitter = models.CharField(max_length=35, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
