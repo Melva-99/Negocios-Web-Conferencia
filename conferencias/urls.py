a8142c4fc935856bdec53f366adb579538ec6bbf
Melva-99 <melvarobertina.83@outlook.com>
2021-06-10 00:39:48 -0600

    Urls del proyecto

--- conferencias/urls.py
+++ conferencias/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('participantes/',views.participantes, name="participantes"),
+    path('participantes/<int:id>/eliminar/', views.eliminar_participante, name='eliminar_participante'),
+    path('participantes/<int:id>/editar/', views.editar_participante, name='editar_participante'),
+    path('conferencistas/',views.conferencistas, name="conferencistas"),
+    path('conferencistas/<int:id>/eliminar/', views.eliminar_conferencista, name='eliminar_conferencista'),
+    path('conferencistas/<int:id>/editar/', views.editar_conferencista, name='editar_conferencista'),
+
 ]
